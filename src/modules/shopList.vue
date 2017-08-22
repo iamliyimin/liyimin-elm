d9e211ba81e0ea89a04c7f97e6be3e170402aae9
iamliyimin <2427802135@qq.com>
2017-08-22 08:04:42 +0800

    五星好评

--- src/modules/shopList.vue
+++ src/modules/shopList.vue
@@ -16,7 +16,14 @@
         <ul class="shopDescription">
           <li class="shopTitle">
             <span class="brand">品牌</span>
-            <span class="shopName">商家1111111</span>
+            <span class="shopName">潮客汇潮全牛火锅（萝岗店）</span>
+            <span class="hasType">票</span>
+          </li>
+          <li class="shopCommit">
+            <p class="star">
+              <span v-for="(itemClass,index) in itemClasses" :class="itemClass" class="star-item" track-by="index">
+              </span><!--性能优化 track-by 数据不改变时不会重新渲染-->
+            </p>
           </li>
         </ul>
       </div>
@@ -25,15 +32,44 @@
 </template>
 
 <script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {
+        score: '3.3'
+      }
+    },
+    methods: {
+    },
+    computed: { // 计算属性
+      itemClasses () {
+        let result = []
+        let score = Math.floor(this.score * 2) / 2
+        let hasDecimal = score % 1 !== 0
+        let integer = Math.floor(score)
+        for (let i = 0; i < integer; i++) {
+          result.push('on')
+        }
+        if (hasDecimal) {
+          result.push('half')
+        }
+        while (result.length < 5) {
+          result.push('off')
+        }
+        return result
+      }
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus-loader" rel="stylesheet/stylus">
+/*商家列表*/
 .shopList
   width: 100%
   margin-top: 0.12rem
   background: #FFFFFF
-  /*border-top: 0.0001rem solid rgba(148,148,148,0.2)*/
+  border-top: 0.0001rem solid rgba(148,148,148,0.2)
+  /*附件商家标题*/
   .shops
     width: 100%
     height: 0.4rem
@@ -45,15 +81,18 @@
     img
       width: 0.23rem
       height: 0.23rem
+  /*商家项*/
   .shopItem
     width: 100%
     height: 2rem
     border-bottom: 0.0001rem solid rgba(148,148,148,0.2)
+    /*具体某个商家*/
     .ashop
       overflow: hidden
       width: 95%
       margin: 0 auto
       padding: 0
+      /*具体某个商家的logo*/
       .shopImg
         display: inline-block
         float: left
@@ -69,6 +108,7 @@
           margin: 0
           width: 100%
           height: 100%
+      /*具体某个商家的店面信息*/
       .shopDescription
         overflow: hidden
         list-style: none
@@ -78,7 +118,6 @@
         margin: 0
         padding: 0
         font-size: 0.1rem
-        /*background: red*/
         li
           overflow: hidden
           display: inline-block
@@ -88,6 +127,7 @@
         .shopTitle
           text-align: left
           height: 0.3rem
+          /*具体某个商家是否是品牌店*/
           .brand
             display: inline-block
             margin: 0.04rem
@@ -101,13 +141,49 @@
             font-weight: bold
             border-radius: 0.03rem
             background: #FFD930
+          /*具体某个商家的店名*/
           .shopName
             display: inline-block
             float: left
-            width: 1.5rem
             height:0.23rem
             padding-left: 0.03rem
             line-height: 0.23rem
             font-size: 0.17rem
             font-weight: bold
+          /*具体某个商家是否有外卖保或者开发票*/
+          .hasType
+            display: inline-block
+            float: right
+            height:0.17rem
+            padding: 0.02rem
+            margin-top: 0.03rem
+            line-height: 0.13rem
+            font-size: 0.1rem
+            text-align: center
+            color: rgba(148,148,148,1)
+            border-radius: 0.03rem
+            box-sizing: border-box
+            border: 0.00001rem solid rgba(148,148,148,0.2)
+            background: rgba(148,148,148,0.08)
+        .shopCommit
+          overflow: hidden
+          text-align: left
+          height: 0.2rem
+          line-height: 0.2rem
+          /*background: red*/
+          .star
+            font-size: 0
+          .star-item
+            display: inline-block
+            background-repeat: no-repeat
+            width: 20px
+            height: 20px
+            /*margin-right: 22px*/
+            background-size: 100%
+          .star-item.on
+            background-image: url(../assets/images/shoplist/on.png)
+          .star-item.half
+            background-image: url(../assets/images/shoplist/half.png)
+          .star-item.off
+            background-image: url(../assets/images/shoplist/off.png)
 </style>
